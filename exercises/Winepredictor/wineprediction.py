fe62c5c6b028424cb0bd2d81f1fd57add39a5ca8
Didrik <didrik@Didriks-MacBook-Pro.local>
2020-08-28 14:06:32 +0200

    made model of prive versus points, and plotted result

--- exercises/Winepredictor/wineprediction.py
+++ exercises/Winepredictor/wineprediction.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from IPython.display import display
+from sklearn.linear_model import LinearRegression 
+from sklearn.metrics import mean_squared_error, r2_score, mean_squared_log_error, mean_absolute_error
 import os
 
 # Where to save the figures and data files
@@ -26,13 +28,37 @@ def data_path(dat_id):
     return os.path.join(DATA_ID, dat_id)
 
 def save_fig(fig_id):
-    plt.savefig(image_path(fig_id) + ".png", format='png')
+    plt.savefig(image_path(fig_id) + ".png", format='png', dpi=300)
 
 infile = open(data_path("winemag-data-130k-v2.csv"),'r')
 
 #cleaning data
 wine_data = pd.read_csv(infile, index_col=[0])
-wine_data.dropna()
+wine_data = wine_data.dropna()
+wine_data = wine_data[wine_data.price < 1000]
 
-country = wine_data['country']
+# assign relevant data
+points = wine_data['points']
 price = wine_data['price']
+
+X = np.zeros((len(price),2))
+X[:,0] = 1
+X[:,1] = price
+
+linreg = LinearRegression().fit(X,points)
+points_predict = linreg.predict(X)
+
+# The mean squared error                               
+print("Mean squared error: %.2f" % mean_squared_error(points, points_predict))
+# Explained variance score: 1 is perfect prediction                                 
+print('Variance score: %.2f' % r2_score(points, points_predict))
+# Mean absolute error                                                           
+print('Mean absolute error: %.2f' % mean_absolute_error(points, points_predict))
+plt.plot(price, points_predict, "r-")
+plt.plot(price, points ,'ro')
+plt.axis([wine_data.min()['price'], wine_data.max()['price'], wine_data.min()['points'], wine_data.max()['points']])
+plt.xlabel(r'$price$')
+plt.ylabel(r'$points$')
+plt.title(r'Linear Regression fit ')
+save_fig("wine_price_points_predict")
+plt.show()
